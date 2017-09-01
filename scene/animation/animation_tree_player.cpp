cardboardcomputer/godot

commit 1afe50538b6f6cce40110e9a135cb3199fab4cbb

Fix some argument ordering, closes #10010

scene/animation/animation_tree_player.cpp
@@ -1002,7 +1002,7 @@ void AnimationTreePlayer::animation_node_set_master_animation(const StringName &
 		_update_sources();
 }
 
-void AnimationTreePlayer::animation_node_set_filter_path(const StringName &p_node, const NodePath &p_track_path, bool p_filter) {
+void AnimationTreePlayer::animation_node_set_filter_path(const StringName &p_node, const NodePath &p_track_path, bool p_track_path) {
 
 	GET_NODE(NODE_ANIMATION, AnimationNode);
 

drivers/gles3/rasterizer_scene_gles3.h
@@ -531,7 +531,7 @@ public:
 	virtual void environment_set_fog(RID p_env, bool p_enable, float p_begin, float p_end, RID p_gradient_texture);
 
 	virtual void environment_set_ssr(RID p_env, bool p_enable, int p_max_steps, float p_fade_in, float p_fade_out, float p_depth_tolerance, bool p_roughness);
-	virtual void environment_set_ssao(RID p_env, bool p_enable, float p_radius, float p_radius2, float p_intensity2, float p_intensity, float p_bias, float p_light_affect, const Color &p_color, bool p_blur);
+	virtual void environment_set_ssao(RID p_env, bool p_enable, float p_radius, float p_intensity, float p_radius2, float p_intensity2, float p_bias, float p_light_affect, const Color &p_color, bool p_blur);
 
 	virtual void environment_set_tonemap(RID p_env, VS::EnvironmentToneMapper p_tone_mapper, float p_exposure, float p_white, bool p_auto_exposure, float p_min_luminance, float p_max_luminance, float p_auto_exp_speed, float p_auto_exp_scale);
 

scene/animation/animation_tree_player.h
@@ -347,7 +347,7 @@ public:
 	void animation_node_set_master_animation(const StringName &p_node, const String &p_master_animation);
 	String animation_node_get_master_animation(const StringName &p_node) const;
 
-	void animation_node_set_filter_path(const StringName &p_node, const NodePath &p_filter, bool p_enable);
+	void animation_node_set_filter_path(const StringName &p_node, const NodePath &p_track_path, bool p_filter);
 	void animation_node_set_get_filtered_paths(const StringName &p_node, List<NodePath> *r_paths) const;
 	bool animation_node_is_path_filtered(const StringName &p_node, const NodePath &p_path) const;
 

commit 304df7976b724cc64728f0a2fda617b1a28b9105

Fix AnimationTreePlayer bogus argument name
Regression from 1afe50538b6f6cce40110e9a135cb3199fab4cbb.

scene/animation/animation_tree_player.cpp
@@ -1002,7 +1002,7 @@ void AnimationTreePlayer::animation_node_set_master_animation(const StringName &
 		_update_sources();
 }
 
-void AnimationTreePlayer::animation_node_set_filter_path(const StringName &p_node, const NodePath &p_track_path, bool p_track_path) {
+void AnimationTreePlayer::animation_node_set_filter_path(const StringName &p_node, const NodePath &p_track_path, bool p_filter) {
 
 	GET_NODE(NODE_ANIMATION, AnimationNode);
 
cardboardcomputer/godot

commit 7119f9edef29a5e48937760a99befe27e1a3f638

-Ability to and unwrap lightmap coordinates on import -Added unwrap functionality to Mesh -Ability to display and debug mesh UVs -Added multiline draw, so it's easier and faster to draw UVs -Many fixes to SurfaceTool -Fixes to Thekla Unwrap, but it's a piece of ass and it keeps crashing. Will have to go away

scene/resources/mesh.cpp
@@ -28,10 +28,10 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 #include "mesh.h"
+#include "pair.h"
 #include "scene/resources/concave_polygon_shape.h"
 #include "scene/resources/convex_polygon_shape.h"
 #include "surface_tool.h"
-
 void Mesh::_clear_triangle_mesh() const {
 
 	triangle_mesh.unref();
@@ -413,7 +413,20 @@ Ref<Mesh> Mesh::create_outline(float p_margin) const {
 	return newmesh;
 }
 
+void Mesh::set_lightmap_size_hint(const Vector2 &p_size) {
+	lightmap_size_hint = p_size;
+}
+
+Size2 Mesh::get_lightmap_size_hint() const {
+	return lightmap_size_hint;
+}
+
 void Mesh::_bind_methods() {
+
+	ClassDB::bind_method(D_METHOD("set_lightmap_size_hint", "size"), &Mesh::set_lightmap_size_hint);
+	ClassDB::bind_method(D_METHOD("get_lightmap_size_hint"), &Mesh::get_lightmap_size_hint);
+
+	ADD_PROPERTYNZ(PropertyInfo(Variant::VECTOR2, "lightmap_size_hint"), "set_lightmap_size_hint", "get_lightmap_size_hint");
 
 	BIND_ENUM_CONSTANT(PRIMITIVE_POINTS);
 	BIND_ENUM_CONSTANT(PRIMITIVE_LINES);
@@ -1033,6 +1046,200 @@ void ArrayMesh::regen_normalmaps() {
 		surfs[i]->generate_tangents();
 		surfs[i]->commit(Ref<ArrayMesh>(this));
 	}
+}
+
+//dirty hack
+bool (*array_mesh_lightmap_unwrap_callback)(float p_texel_size, const float *p_vertices, const float *p_normals, int p_vertex_count, const int *p_indices, const int *p_face_materials, int p_index_count, float **r_uv, int **r_vertex, int *r_vertex_count, int **r_index, int *r_index_count, int *r_size_hint_x, int *r_size_hint_y) = NULL;
+
+struct ArrayMeshLightmapSurface {
+
+	Ref<Material> material;
+	Vector<SurfaceTool::Vertex> vertices;
+	Mesh::PrimitiveType primitive;
+	uint32_t format;
+};
+
+Error ArrayMesh::lightmap_unwrap(const Transform &p_base_transform, float p_texel_size) {
+
+	ERR_FAIL_COND_V(!array_mesh_lightmap_unwrap_callback, ERR_UNCONFIGURED);
+	ERR_EXPLAIN("Can't unwrap mesh with blend shapes");
+	ERR_FAIL_COND_V(blend_shapes.size() != 0, ERR_UNAVAILABLE);
+
+	Vector<float> vertices;
+	Vector<float> normals;
+	Vector<int> indices;
+	Vector<int> face_materials;
+	Vector<float> uv;
+	Vector<Pair<int, int> > uv_index;
+
+	Vector<ArrayMeshLightmapSurface> surfaces;
+	for (int i = 0; i < get_surface_count(); i++) {
+		ArrayMeshLightmapSurface s;
+		s.primitive = surface_get_primitive_type(i);
+
+		if (s.primitive != Mesh::PRIMITIVE_TRIANGLES) {
+			ERR_EXPLAIN("Only triangles are supported for lightmap unwrap");
+			ERR_FAIL_V(ERR_UNAVAILABLE);
+		}
+		s.format = surface_get_format(i);
+		if (!(s.format & ARRAY_FORMAT_NORMAL)) {
+			ERR_EXPLAIN("Normals are required for lightmap unwrap");
+			ERR_FAIL_V(ERR_UNAVAILABLE);
+		}
+
+		Array arrays = surface_get_arrays(i);
+		s.material = surface_get_material(i);
+		s.vertices = SurfaceTool::create_vertex_array_from_triangle_arrays(arrays);
+
+		PoolVector<Vector3> rvertices = arrays[Mesh::ARRAY_VERTEX];
+		int vc = rvertices.size();
+		PoolVector<Vector3>::Read r = rvertices.read();
+
+		PoolVector<Vector3> rnormals = arrays[Mesh::ARRAY_NORMAL];
+		PoolVector<Vector3>::Read rn = rnormals.read();
+
+		int vertex_ofs = vertices.size() / 3;
+
+		vertices.resize((vertex_ofs + vc) * 3);
+		normals.resize((vertex_ofs + vc) * 3);
+		uv_index.resize(vertex_ofs + vc);
+
+		for (int j = 0; j < vc; j++) {
+
+			Vector3 v = p_base_transform.xform(r[j]);
+
+			vertices[(j + vertex_ofs) * 3 + 0] = v.x;
+			vertices[(j + vertex_ofs) * 3 + 1] = v.y;
+			vertices[(j + vertex_ofs) * 3 + 2] = v.z;
+			normals[(j + vertex_ofs) * 3 + 0] = rn[j].x;
+			normals[(j + vertex_ofs) * 3 + 1] = rn[j].y;
+			normals[(j + vertex_ofs) * 3 + 2] = rn[j].z;
+			uv_index[j + vertex_ofs] = Pair<int, int>(i, j);
+		}
+
+		PoolVector<int> rindices = arrays[Mesh::ARRAY_INDEX];
+		int ic = rindices.size();
+		int index_ofs = indices.size();
+
+		if (ic == 0) {
+			indices.resize(index_ofs + vc);
+			face_materials.resize((index_ofs + vc) / 3);
+			for (int j = 0; j < vc; j++) {
+				indices[index_ofs + j] = vertex_ofs + j;
+			}
+			for (int j = 0; j < vc / 3; j++) {
+				face_materials[(index_ofs / 3) + j] = i;
+			}
+
+		} else {
+			PoolVector<int>::Read ri = rindices.read();
+			indices.resize(index_ofs + ic);
+			face_materials.resize((index_ofs + ic) / 3);
+			for (int j = 0; j < ic; j++) {
+				indices[index_ofs + j] = vertex_ofs + ri[j];
+			}
+			for (int j = 0; j < ic / 3; j++) {
+				face_materials[(index_ofs / 3) + j] = i;
+			}
+		}
+
+		surfaces.push_back(s);
+	}
+
+	//unwrap
+
+	float *gen_uvs;
+	int *gen_vertices;
+	int *gen_indices;
+	int gen_vertex_count;
+	int gen_index_count;
+	int size_x;
+	int size_y;
+
+	bool ok = array_mesh_lightmap_unwrap_callback(p_texel_size, vertices.ptr(), normals.ptr(), vertices.size() / 3, indices.ptr(), face_materials.ptr(), indices.size(), &gen_uvs, &gen_vertices, &gen_vertex_count, &gen_indices, &gen_index_count, &size_x, &size_y);
+
+	if (!ok) {
+		return ERR_CANT_CREATE;
+	}
+
+	//remove surfaces
+	while (get_surface_count()) {
+		surface_remove(0);
+	}
+
+	//create surfacetools for each surface..
+	Vector<Ref<SurfaceTool> > surfaces_tools;
+
+	for (int i = 0; i < surfaces.size(); i++) {
+		Ref<SurfaceTool> st;
+		st.instance();
+		st->begin(Mesh::PRIMITIVE_TRIANGLES);
+		st->set_material(surfaces[i].material);
+		surfaces_tools.push_back(st); //stay there
+	}
+
+	print_line("gen indices: " + itos(gen_index_count));
+	//go through all indices
+	for (int i = 0; i < gen_index_count; i += 3) {
+
+		ERR_FAIL_INDEX_V(gen_vertices[gen_indices[i + 0]], uv_index.size(), ERR_BUG);
+		ERR_FAIL_INDEX_V(gen_vertices[gen_indices[i + 1]], uv_index.size(), ERR_BUG);
+		ERR_FAIL_INDEX_V(gen_vertices[gen_indices[i + 2]], uv_index.size(), ERR_BUG);
+
+		ERR_FAIL_COND_V(uv_index[gen_vertices[gen_indices[i + 0]]].first != uv_index[gen_vertices[gen_indices[i + 1]]].first || uv_index[gen_vertices[gen_indices[i + 0]]].first != uv_index[gen_vertices[gen_indices[i + 2]]].first, ERR_BUG);
+
+		int surface = uv_index[gen_vertices[gen_indices[i + 0]]].first;
+
+		for (int j = 0; j < 3; j++) {
+
+			int vertex_idx = gen_vertices[gen_indices[i + j]];
+
+			SurfaceTool::Vertex v = surfaces[surface].vertices[uv_index[gen_vertices[gen_indices[i + j]]].second];
+
+			if (surfaces[surface].format & ARRAY_FORMAT_COLOR) {
+				surfaces_tools[surface]->add_color(v.color);
+			}
+			if (surfaces[surface].format & ARRAY_FORMAT_TEX_UV) {
+				surfaces_tools[surface]->add_uv(v.uv);
+			}
+			if (surfaces[surface].format & ARRAY_FORMAT_NORMAL) {
+				surfaces_tools[surface]->add_normal(v.normal);
+			}
+			if (surfaces[surface].format & ARRAY_FORMAT_TANGENT) {
+				Plane t;
+				t.normal = v.tangent;
+				t.d = v.binormal.dot(v.normal.cross(v.tangent)) < 0 ? -1 : 1;
+				surfaces_tools[surface]->add_tangent(t);
+			}
+			if (surfaces[surface].format & ARRAY_FORMAT_BONES) {
+				surfaces_tools[surface]->add_bones(v.bones);
+			}
+			if (surfaces[surface].format & ARRAY_FORMAT_WEIGHTS) {
+				surfaces_tools[surface]->add_weights(v.weights);
+			}
+
+			Vector2 uv2(gen_uvs[gen_indices[i + j] * 2 + 0], gen_uvs[gen_indices[i + j] * 2 + 1]);
+			surfaces_tools[surface]->add_uv2(uv2);
+
+			surfaces_tools[surface]->add_vertex(v.vertex);
+		}
+	}
+
+	//free stuff
+	::free(gen_vertices);
+	::free(gen_indices);
+	::free(gen_uvs);
+
+	//generate surfaces
+
+	for (int i = 0; i < surfaces_tools.size(); i++) {
+		surfaces_tools[i]->index();
+		surfaces_tools[i]->commit(Ref<ArrayMesh>((ArrayMesh *)this), surfaces[i].format);
+	}
+
+	set_lightmap_size_hint(Size2(size_x, size_y));
+
+	return OK;
 }
 
 void ArrayMesh::_bind_methods() {

drivers/gles3/rasterizer_canvas_gles3.cpp
@@ -527,7 +527,9 @@ void RasterizerCanvasGLES3::_canvas_item_render_commands(Item *p_item, Item *cur
 					_draw_generic(GL_TRIANGLE_STRIP, pline->triangles.size(), pline->triangles.ptr(), NULL, pline->triangle_colors.ptr(), pline->triangle_colors.size() == 1);
 #ifdef GLES_OVER_GL
 					glEnable(GL_LINE_SMOOTH);
-					if (pline->lines.size()) {
+					if (pline->multiline) {
+						//needs to be different
+					} else {
 						_draw_generic(GL_LINE_LOOP, pline->lines.size(), pline->lines.ptr(), NULL, pline->line_colors.ptr(), pline->line_colors.size() == 1);
 					}
 					glDisable(GL_LINE_SMOOTH);
@@ -538,7 +540,23 @@ void RasterizerCanvasGLES3::_canvas_item_render_commands(Item *p_item, Item *cur
 					if (pline->antialiased)
 						glEnable(GL_LINE_SMOOTH);
 #endif
-					_draw_generic(GL_LINE_STRIP, pline->lines.size(), pline->lines.ptr(), NULL, pline->line_colors.ptr(), pline->line_colors.size() == 1);
+
+					if (pline->multiline) {
+						int todo = pline->lines.size() / 2;
+						int max_per_call = data.polygon_buffer_size / (sizeof(real_t) * 4);
+						int offset = 0;
+
+						while (todo) {
+							int to_draw = MIN(max_per_call, todo);
+							_draw_generic(GL_LINES, to_draw * 2, &pline->lines.ptr()[offset], NULL, pline->line_colors.size() == 1 ? pline->line_colors.ptr() : &pline->line_colors.ptr()[offset], pline->line_colors.size() == 1);
+							todo -= to_draw;
+							offset += to_draw * 2;
+						}
+
+					} else {
+
+						_draw_generic(GL_LINES, pline->lines.size(), pline->lines.ptr(), NULL, pline->line_colors.ptr(), pline->line_colors.size() == 1);
+					}
 
 #ifdef GLES_OVER_GL
 					if (pline->antialiased)
@@ -1705,6 +1723,7 @@ void RasterizerCanvasGLES3::initialize() {
 		glBindBuffer(GL_ARRAY_BUFFER, data.polygon_buffer);
 		glBufferData(GL_ARRAY_BUFFER, poly_size, NULL, GL_DYNAMIC_DRAW); //allocate max size
 		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		data.polygon_buffer_size = poly_size;
 
 		//quad arrays
 		for (int i = 0; i < 4; i++) {

editor/import/resource_importer_scene.cpp
@@ -47,8 +47,6 @@
 #include "scene/resources/ray_shape.h"
 #include "scene/resources/sphere_shape.h"
 
-
-
 uint32_t EditorSceneImporter::get_import_flags() const {
 
 	if (get_script_instance()) {
@@ -60,79 +58,72 @@ uint32_t EditorSceneImporter::get_import_flags() const {
 void EditorSceneImporter::get_extensions(List<String> *r_extensions) const {
 
 	if (get_script_instance()) {
-		Array arr= get_script_instance()->call("_get_extensions");
-		for(int i=0;i<arr.size();i++) {
+		Array arr = get_script_instance()->call("_get_extensions");
+		for (int i = 0; i < arr.size(); i++) {
 			r_extensions->push_back(arr[i]);
 		}
 		return;
 	}
 
 	ERR_FAIL();
-
 }
 Node *EditorSceneImporter::import_scene(const String &p_path, uint32_t p_flags, int p_bake_fps, List<String> *r_missing_deps, Error *r_err) {
 
 	if (get_script_instance()) {
-		return get_script_instance()->call("_import_scene",p_path,p_flags,p_bake_fps);
+		return get_script_instance()->call("_import_scene", p_path, p_flags, p_bake_fps);
 	}
 
 	ERR_FAIL_V(NULL);
-
 }
 
-Ref<Animation> EditorSceneImporter::import_animation(const String &p_path, uint32_t p_flags,int p_bake_fps) {
+Ref<Animation> EditorSceneImporter::import_animation(const String &p_path, uint32_t p_flags, int p_bake_fps) {
 
 	if (get_script_instance()) {
-		return get_script_instance()->call("_import_animation",p_path,p_flags);
+		return get_script_instance()->call("_import_animation", p_path, p_flags);
 	}
 
 	ERR_FAIL_V(NULL);
-
 }
 
 //for documenters, these functions are useful when an importer calls an external conversion helper (like, fbx2gltf),
 //and you want to load the resulting file
 
-Node* EditorSceneImporter::import_scene_from_other_importer(const String &p_path, uint32_t p_flags, int p_bake_fps) {
+Node *EditorSceneImporter::import_scene_from_other_importer(const String &p_path, uint32_t p_flags, int p_bake_fps) {
 
-	return ResourceImporterScene::get_singleton()->import_scene_from_other_importer(this,p_path,p_flags,p_bake_fps);
-
+	return ResourceImporterScene::get_singleton()->import_scene_from_other_importer(this, p_path, p_flags, p_bake_fps);
 }
 
 Ref<Animation> EditorSceneImporter::import_animation_from_other_importer(const String &p_path, uint32_t p_flags, int p_bake_fps) {
 
-	return ResourceImporterScene::get_singleton()->import_animation_from_other_importer(this,p_path,p_flags,p_bake_fps);
-
+	return ResourceImporterScene::get_singleton()->import_animation_from_other_importer(this, p_path, p_flags, p_bake_fps);
 }
 
 void EditorSceneImporter::_bind_methods() {
 
-	ClassDB::bind_method(D_METHOD("import_scene_from_other_importer","path","flags","bake_fps"),&EditorSceneImporter::import_scene_from_other_importer);
-	ClassDB::bind_method(D_METHOD("import_animation_from_other_importer","path","flags","bake_fps"),&EditorSceneImporter::import_animation_from_other_importer);
+	ClassDB::bind_method(D_METHOD("import_scene_from_other_importer", "path", "flags", "bake_fps"), &EditorSceneImporter::import_scene_from_other_importer);
+	ClassDB::bind_method(D_METHOD("import_animation_from_other_importer", "path", "flags", "bake_fps"), &EditorSceneImporter::import_animation_from_other_importer);
 
 	BIND_VMETHOD(MethodInfo(Variant::INT, "_get_import_flags"));
 	BIND_VMETHOD(MethodInfo(Variant::ARRAY, "_get_extensions"));
 
-	MethodInfo mi = MethodInfo(Variant::OBJECT, "_import_scene",PropertyInfo(Variant::STRING, "path"), PropertyInfo(Variant::INT, "flags"), PropertyInfo(Variant::INT, "bake_fps"));
-	mi.return_val.class_name="Node";
+	MethodInfo mi = MethodInfo(Variant::OBJECT, "_import_scene", PropertyInfo(Variant::STRING, "path"), PropertyInfo(Variant::INT, "flags"), PropertyInfo(Variant::INT, "bake_fps"));
+	mi.return_val.class_name = "Node";
 	BIND_VMETHOD(mi);
-	mi = MethodInfo(Variant::OBJECT, "_import_animation",PropertyInfo(Variant::STRING, "path"), PropertyInfo(Variant::INT, "flags"), PropertyInfo(Variant::INT, "bake_fps"));
-	mi.return_val.class_name="Animation";
+	mi = MethodInfo(Variant::OBJECT, "_import_animation", PropertyInfo(Variant::STRING, "path"), PropertyInfo(Variant::INT, "flags"), PropertyInfo(Variant::INT, "bake_fps"));
+	mi.return_val.class_name = "Animation";
 	BIND_VMETHOD(mi);
 
-	BIND_CONSTANT( IMPORT_SCENE );
-	BIND_CONSTANT( IMPORT_ANIMATION );
-	BIND_CONSTANT( IMPORT_ANIMATION_DETECT_LOOP );
-	BIND_CONSTANT( IMPORT_ANIMATION_OPTIMIZE );
-	BIND_CONSTANT( IMPORT_ANIMATION_FORCE_ALL_TRACKS_IN_ALL_CLIPS );
-	BIND_CONSTANT( IMPORT_ANIMATION_KEEP_VALUE_TRACKS );
-	BIND_CONSTANT( IMPORT_GENERATE_TANGENT_ARRAYS );
-	BIND_CONSTANT( IMPORT_FAIL_ON_MISSING_DEPENDENCIES );
-	BIND_CONSTANT( IMPORT_MATERIALS_IN_INSTANCES );
-	BIND_CONSTANT( IMPORT_USE_COMPRESSION );
-
+	BIND_CONSTANT(IMPORT_SCENE);
+	BIND_CONSTANT(IMPORT_ANIMATION);
+	BIND_CONSTANT(IMPORT_ANIMATION_DETECT_LOOP);
+	BIND_CONSTANT(IMPORT_ANIMATION_OPTIMIZE);
+	BIND_CONSTANT(IMPORT_ANIMATION_FORCE_ALL_TRACKS_IN_ALL_CLIPS);
+	BIND_CONSTANT(IMPORT_ANIMATION_KEEP_VALUE_TRACKS);
+	BIND_CONSTANT(IMPORT_GENERATE_TANGENT_ARRAYS);
+	BIND_CONSTANT(IMPORT_FAIL_ON_MISSING_DEPENDENCIES);
+	BIND_CONSTANT(IMPORT_MATERIALS_IN_INSTANCES);
+	BIND_CONSTANT(IMPORT_USE_COMPRESSION);
 }
-
 
 /////////////////////////////////
 void EditorScenePostImport::_bind_methods() {
@@ -198,6 +189,10 @@ bool ResourceImporterScene::get_option_visibility(const String &p_option, const 
 	}
 
 	if (p_option == "materials/keep_on_reimport" && int(p_options["materials/storage"]) == 0) {
+		return false;
+	}
+
+	if (p_option == "meshes/lightmap_texel_size" && int(p_options["meshes/light_baking"]) < 2) {
 		return false;
 	}
 
@@ -961,6 +956,36 @@ static String _make_extname(const String &p_str) {
 	return ext_name;
 }
 
+void ResourceImporterScene::_find_meshes(Node *p_node, Map<Ref<ArrayMesh>, Transform> &meshes) {
+
+	List<PropertyInfo> pi;
+	p_node->get_property_list(&pi);
+
+	MeshInstance *mi = Object::cast_to<MeshInstance>(p_node);
+
+	if (mi) {
+
+		Ref<ArrayMesh> mesh = mi->get_mesh();
+
+		if (mesh.is_valid() && !meshes.has(mesh)) {
+			Spatial *s = mi;
+			while (s->get_parent_spatial()) {
+				s = s->get_parent_spatial();
+			}
+
+			if (s == mi) {
+				meshes[mesh] = s->get_transform();
+			} else {
+				meshes[mesh] = s->get_transform() * mi->get_relative_transform(s);
+			}
+		}
+	}
+	for (int i = 0; i < p_node->get_child_count(); i++) {
+
+		_find_meshes(p_node->get_child(i), meshes);
+	}
+}
+
 void ResourceImporterScene::_make_external_resources(Node *p_node, const String &p_base_path, bool p_make_animations, bool p_keep_animations, bool p_make_materials, bool p_keep_materials, bool p_make_meshes, Map<Ref<Animation>, Ref<Animation> > &p_animations, Map<Ref<Material>, Ref<Material> > &p_materials, Map<Ref<ArrayMesh>, Ref<ArrayMesh> > &p_meshes) {
 
 	List<PropertyInfo> pi;
@@ -1140,7 +1165,8 @@ void ResourceImporterScene::get_import_options(List<ImportOption> *r_options, in
 	r_options->push_back(ImportOption(PropertyInfo(Variant::BOOL, "meshes/compress"), true));
 	r_options->push_back(ImportOption(PropertyInfo(Variant::BOOL, "meshes/ensure_tangents"), true));
 	r_options->push_back(ImportOption(PropertyInfo(Variant::INT, "meshes/storage", PROPERTY_HINT_ENUM, "Built-In,Files"), meshes_out ? 1 : 0));
-	r_options->push_back(ImportOption(PropertyInfo(Variant::INT, "meshes/light_baking", PROPERTY_HINT_ENUM, "Disabled,Enable"), 0));
+	r_options->push_back(ImportOption(PropertyInfo(Variant::INT, "meshes/light_baking", PROPERTY_HINT_ENUM, "Disabled,Enable,Gen Lightmaps"), 0));
+	r_options->push_back(ImportOption(PropertyInfo(Variant::REAL, "meshes/lightmap_texel_size", PROPERTY_HINT_RANGE, "0.001,100,0.001"), 0.05));
 	r_options->push_back(ImportOption(PropertyInfo(Variant::BOOL, "external_files/store_in_subdir"), false));
 	r_options->push_back(ImportOption(PropertyInfo(Variant::BOOL, "animation/import", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_UPDATE_ALL_IF_MODIFIED), true));
 	r_options->push_back(ImportOption(PropertyInfo(Variant::REAL, "animation/fps", PROPERTY_HINT_RANGE, "1,120,1"), 15));
@@ -1171,17 +1197,16 @@ void ResourceImporterScene::_replace_owner(Node *p_node, Node *p_scene, Node *p_
 		Node *n = p_node->get_child(i);
 		_replace_owner(n, p_scene, p_new_owner);
 	}
-
 }
 
-Node* ResourceImporterScene::import_scene_from_other_importer(EditorSceneImporter *p_exception,const String &p_path, uint32_t p_flags, int p_bake_fps) {
+Node *ResourceImporterScene::import_scene_from_other_importer(EditorSceneImporter *p_exception, const String &p_path, uint32_t p_flags, int p_bake_fps) {
 
 	Ref<EditorSceneImporter> importer;
 	String ext = p_path.get_extension().to_lower();
 
 	for (Set<Ref<EditorSceneImporter> >::Element *E = importers.front(); E; E = E->next()) {
 
-		if (E->get().ptr()==p_exception)
+		if (E->get().ptr() == p_exception)
 			continue;
 		List<String> extensions;
 		E->get()->get_extensions(&extensions);
@@ -1199,42 +1224,41 @@ Node* ResourceImporterScene::import_scene_from_other_importer(EditorSceneImporte
 			break;
 	}
 
-	ERR_FAIL_COND_V(!importer.is_valid(),NULL);
+	ERR_FAIL_COND_V(!importer.is_valid(), NULL);
 
 	List<String> missing;
 	Error err;
-	return importer->import_scene(p_path,p_flags,p_bake_fps,&missing,&err);
+	return importer->import_scene(p_path, p_flags, p_bake_fps, &missing, &err);
 }
 
-Ref<Animation> ResourceImporterScene::import_animation_from_other_importer(EditorSceneImporter *p_exception,const String &p_path, uint32_t p_flags, int p_bake_fps) {
+Ref<Animation> ResourceImporterScene::import_animation_from_other_importer(EditorSceneImporter *p_exception, const String &p_path, uint32_t p_flags, int p_bake_fps) {
 
+	Ref<EditorSceneImporter> importer;
+	String ext = p_path.get_extension().to_lower();
 
-	     Ref<EditorSceneImporter> importer;
-	     String ext = p_path.get_extension().to_lower();
+	for (Set<Ref<EditorSceneImporter> >::Element *E = importers.front(); E; E = E->next()) {
 
-	     for (Set<Ref<EditorSceneImporter> >::Element *E = importers.front(); E; E = E->next()) {
+		if (E->get().ptr() == p_exception)
+			continue;
+		List<String> extensions;
+		E->get()->get_extensions(&extensions);
 
-			if (E->get().ptr()==p_exception)
-	     continue;
-		     List<String> extensions;
-		     E->get()->get_extensions(&extensions);
+		for (List<String>::Element *F = extensions.front(); F; F = F->next()) {
 
-		     for (List<String>::Element *F = extensions.front(); F; F = F->next()) {
+			if (F->get().to_lower() == ext) {
 
-			     if (F->get().to_lower() == ext) {
+				importer = E->get();
+				break;
+			}
+		}
 
-				     importer = E->get();
-				     break;
-			     }
-		     }
+		if (importer.is_valid())
+			break;
+	}
 
-		     if (importer.is_valid())
-			     break;
-	     }
+	ERR_FAIL_COND_V(!importer.is_valid(), NULL);
 
-	     ERR_FAIL_COND_V(!importer.is_valid(),NULL);
-
-	     return importer->import_animation(p_path,p_flags,p_bake_fps);
+	return importer->import_animation(p_path, p_flags, p_bake_fps);
 }
 
 Error ResourceImporterScene::import(const String &p_source_file, const String &p_save_path, const Map<StringName, Variant> &p_options, List<String> *r_platform_variants, List<String> *r_gen_files) {
@@ -1368,6 +1392,37 @@ Error ResourceImporterScene::import(const String &p_source_file, const String &p
 			memdelete(da);
 			ERR_FAIL_COND_V(err != OK && err != ERR_ALREADY_EXISTS, err);
 			base_path = base_path.plus_file(subdir_name);
+		}
+	}
+
+	if (light_bake_mode == 2 /* || generate LOD */) {
+
+		Map<Ref<ArrayMesh>, Transform> meshes;
+		_find_meshes(scene, meshes);
+
+		if (light_bake_mode == 2) {
+
+			float texel_size = p_options["meshes/lightmap_texel_size"];
+			texel_size = MAX(0.001, texel_size);
+
+			EditorProgress progress("gen_lightmaps", TTR("Generating Lightmaps"), meshes.size());
+			int step = 0;
+			for (Map<Ref<ArrayMesh>, Transform>::Element *E = meshes.front(); E; E = E->next()) {
+
+				Ref<ArrayMesh> mesh = E->key();
+				String name = mesh->get_name();
+				if (name == "") { //should not happen but..
+					name = "Mesh " + itos(step);
+				}
+
+				progress.step(TTR("Generating for Mesh: ") + name + " (" + itos(step) + "/" + itos(meshes.size()) + ")", step);
+
+				Error err = mesh->lightmap_unwrap(E->get(), texel_size);
+				if (err != OK) {
+					EditorNode::add_io_error("Mesh '" + name + "' failed lightmap generation. Please fix geometry.");
+				}
+				step++;
+			}
 		}
 	}
 

editor/import/resource_importer_scene.h
@@ -40,13 +40,13 @@ class Material;
 class EditorSceneImporter : public Reference {
 
 	GDCLASS(EditorSceneImporter, Reference);
-protected:
 
+protected:
 	static void _bind_methods();
 
-
-	Node* import_scene_from_other_importer(const String &p_path, uint32_t p_flags, int p_bake_fps);
+	Node *import_scene_from_other_importer(const String &p_path, uint32_t p_flags, int p_bake_fps);
 	Ref<Animation> import_animation_from_other_importer(const String &p_path, uint32_t p_flags, int p_bake_fps);
+
 public:
 	enum ImportFlags {
 		IMPORT_SCENE = 1,
@@ -62,11 +62,10 @@ public:
 
 	};
 
-
 	virtual uint32_t get_import_flags() const;
 	virtual void get_extensions(List<String> *r_extensions) const;
 	virtual Node *import_scene(const String &p_path, uint32_t p_flags, int p_bake_fps, List<String> *r_missing_deps, Error *r_err = NULL);
-	virtual Ref<Animation> import_animation(const String &p_path, uint32_t p_flags,int p_bake_fps);
+	virtual Ref<Animation> import_animation(const String &p_path, uint32_t p_flags, int p_bake_fps);
 
 	EditorSceneImporter() {}
 };
@@ -136,6 +135,8 @@ public:
 	virtual bool get_option_visibility(const String &p_option, const Map<StringName, Variant> &p_options) const;
 	virtual int get_import_order() const { return 100; } //after everything
 
+	void _find_meshes(Node *p_node, Map<Ref<ArrayMesh>, Transform> &meshes);
+
 	void _make_external_resources(Node *p_node, const String &p_base_path, bool p_make_animations, bool p_keep_animations, bool p_make_materials, bool p_keep_materials, bool p_make_meshes, Map<Ref<Animation>, Ref<Animation> > &p_animations, Map<Ref<Material>, Ref<Material> > &p_materials, Map<Ref<ArrayMesh>, Ref<ArrayMesh> > &p_meshes);
 
 	Node *_fix_node(Node *p_node, Node *p_root, Map<Ref<ArrayMesh>, Ref<Shape> > &collision_map, LightBakeMode p_light_bake_mode);
@@ -147,8 +148,8 @@ public:
 
 	virtual Error import(const String &p_source_file, const String &p_save_path, const Map<StringName, Variant> &p_options, List<String> *r_platform_variants, List<String> *r_gen_files = NULL);
 
-	Node* import_scene_from_other_importer(EditorSceneImporter *p_exception,const String &p_path, uint32_t p_flags, int p_bake_fps);
-	Ref<Animation> import_animation_from_other_importer(EditorSceneImporter *p_exception,const String &p_path, uint32_t p_flags, int p_bake_fps);
+	Node *import_scene_from_other_importer(EditorSceneImporter *p_exception, const String &p_path, uint32_t p_flags, int p_bake_fps);
+	Ref<Animation> import_animation_from_other_importer(EditorSceneImporter *p_exception, const String &p_path, uint32_t p_flags, int p_bake_fps);
 
 	ResourceImporterScene();
 };

commit d25da626e17829b20d67625a8d618fbcea3f6097

Fix build in ArrayMesh
Bug introduced in 7119f9edef29a5e48937760a99befe27e1a3f638.

scene/resources/mesh.cpp
@@ -28,10 +28,14 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 #include "mesh.h"
+
 #include "pair.h"
 #include "scene/resources/concave_polygon_shape.h"
 #include "scene/resources/convex_polygon_shape.h"
 #include "surface_tool.h"
+
+#include <stdlib.h>
+
 void Mesh::_clear_triangle_mesh() const {
 
 	triangle_mesh.unref();
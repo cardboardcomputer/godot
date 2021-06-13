godot-extended-libraries/godot-fire

Add stereoscopic rendering through multiview

servers/rendering/renderer_scene.h
@@ -189,7 +189,7 @@ public:
 
 	virtual RID render_buffers_create() = 0;
 
-	virtual void render_buffers_configure(RID p_render_buffers, RID p_render_target, int p_width, int p_height, RS::ViewportMSAA p_msaa, RS::ViewportScreenSpaceAA p_screen_space_aa, bool p_use_debanding) = 0;
+	virtual void render_buffers_configure(RID p_render_buffers, RID p_render_target, int p_width, int p_height, RS::ViewportMSAA p_msaa, RS::ViewportScreenSpaceAA p_screen_space_aa, bool p_use_debanding, uint32_t p_view_count) = 0;
 
 	virtual void gi_set_use_half_resolution(bool p_enable) = 0;
 
@@ -201,8 +201,7 @@ public:
 	virtual void sdfgi_set_debug_probe_select(const Vector3 &p_position, const Vector3 &p_dir) = 0;
 
 	virtual void render_empty_scene(RID p_render_buffers, RID p_scenario, RID p_shadow_atlas) = 0;
-	virtual void render_camera(RID p_render_buffers, RID p_camera, RID p_scenario, RID p_viewport, Size2 p_viewport_size, float p_lod_threshold, RID p_shadow_atlas) = 0;
-	virtual void render_camera(RID p_render_buffers, Ref<XRInterface> &p_interface, XRInterface::Eyes p_eye, RID p_camera, RID p_scenario, RID p_viewport, Size2 p_viewport_size, float p_lod_threshold, RID p_shadow_atlas) = 0;
+	virtual void render_camera(RID p_render_buffers, RID p_camera, RID p_scenario, RID p_viewport, Size2 p_viewport_size, float p_lod_threshold, RID p_shadow_atlas, Ref<XRInterface> &p_interface) = 0;
 
 	virtual void update() = 0;
 	virtual void render_probes() = 0;

servers/rendering/renderer_scene_render.cpp
@@ -29,3 +29,151 @@
 /*************************************************************************/
 
 #include "renderer_scene_render.h"
+
+void RendererSceneRender::ViewData::set_single_view(const Transform p_transform, const CameraMatrix p_projection, bool p_is_ortogonal, bool p_vaspect) {
+	view_count = 1;
+	is_ortogonal = p_is_ortogonal;
+	vaspect = p_vaspect;
+
+	main_transform = p_transform;
+	main_projection = p_projection;
+	view_projection[0] = p_projection;
+}
+
+void RendererSceneRender::ViewData::set_stereo_view(uint32_t p_view_count, const Transform *p_transforms, const CameraMatrix *p_projections, bool p_is_ortogonal, bool p_vaspect) {
+	ERR_FAIL_COND_MSG(p_view_count != 2, "Incorrect view count for stereoscopic view");
+
+	view_count = p_view_count;
+	is_ortogonal = p_is_ortogonal;
+	vaspect = p_vaspect;
+	Vector<Plane> planes[2];
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Figure out our center transform
+
+	// 1. obtain our planes
+	for (uint32_t v = 0; v < view_count; v++) {
+		planes[v] = p_projections[v].get_projection_planes(p_transforms[v]);
+	}
+
+	// 2. average and normalize plane normals to obtain z vector, cross them to obtain y vector, and from there the x vector for combined camera basis.
+	Vector3 n0 = planes[0][CameraMatrix::PLANE_LEFT].normal;
+	Vector3 n1 = planes[1][CameraMatrix::PLANE_RIGHT].normal;
+	Vector3 z = (n0 + n1).normalized();
+	Vector3 y = n0.cross(n1).normalized();
+	Vector3 x = y.cross(z).normalized();
+	y = z.cross(x).normalized();
+	main_transform.basis.set(x, y, z);
+
+	// 3. create a horizon plane with one of the eyes and the up vector as normal.
+	Plane horizon(p_transforms[0].origin, y);
+
+	// 4. Intersect horizon, left and right to obtain the combined camera origin.
+	ERR_FAIL_COND_MSG(
+			!horizon.intersect_3(planes[0][CameraMatrix::PLANE_LEFT], planes[1][CameraMatrix::PLANE_RIGHT], &main_transform.origin), "Can't determine camera origin");
+
+	// handy to have the inverse of the transform we just build
+	Transform main_transform_inv = main_transform.inverse();
+
+	// 5. figure out far plane, this could use some improvement, we may have our far plane too close like this, not sure if this matters
+	Vector3 far_center = (planes[0][CameraMatrix::PLANE_FAR].center() + planes[1][CameraMatrix::PLANE_FAR].center()) * 0.5;
+	Plane far(far_center, -z);
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Figure out our top/bottom planes
+
+	// 6. Intersect far and left planes with top planes from both eyes, save the point with highest y as top_left.
+	Vector3 top_left, other;
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[0][CameraMatrix::PLANE_LEFT], planes[0][CameraMatrix::PLANE_TOP], &top_left), "Can't determine left camera far/left/top vector");
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[1][CameraMatrix::PLANE_LEFT], planes[1][CameraMatrix::PLANE_TOP], &other), "Can't determine right camera far/left/top vector");
+	if (y.dot(top_left) < y.dot(other)) {
+		top_left = other;
+	}
+
+	// 7. Intersect far and left planes with bottom planes from both eyes, save the point with lowest y as bottom_left.
+	Vector3 bottom_left;
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[0][CameraMatrix::PLANE_LEFT], planes[0][CameraMatrix::PLANE_BOTTOM], &bottom_left), "Can't determine left camera far/left/bottom vector");
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[1][CameraMatrix::PLANE_LEFT], planes[1][CameraMatrix::PLANE_BOTTOM], &other), "Can't determine right camera far/left/bottom vector");
+	if (y.dot(other) < y.dot(bottom_left)) {
+		bottom_left = other;
+	}
+
+	// 8. Intersect far and right planes with top planes from both eyes, save the point with highest y as top_right.
+	Vector3 top_right;
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[0][CameraMatrix::PLANE_RIGHT], planes[0][CameraMatrix::PLANE_TOP], &top_right), "Can't determine left camera far/right/top vector");
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[1][CameraMatrix::PLANE_RIGHT], planes[1][CameraMatrix::PLANE_TOP], &other), "Can't determine right camera far/right/top vector");
+	if (y.dot(top_right) < y.dot(other)) {
+		top_right = other;
+	}
+
+	//  9. Intersect far and right planes with bottom planes from both eyes, save the point with lowest y as bottom_right.
+	Vector3 bottom_right;
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[0][CameraMatrix::PLANE_RIGHT], planes[0][CameraMatrix::PLANE_BOTTOM], &bottom_right), "Can't determine left camera far/right/bottom vector");
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[1][CameraMatrix::PLANE_RIGHT], planes[1][CameraMatrix::PLANE_BOTTOM], &other), "Can't determine right camera far/right/bottom vector");
+	if (y.dot(other) < y.dot(bottom_right)) {
+		bottom_right = other;
+	}
+
+	// 10. Create top plane with these points: camera origin, top_left, top_right
+	Plane top(main_transform.origin, top_left, top_right);
+
+	// 11. Create bottom plane with these points: camera origin, bottom_left, bottom_right
+	Plane bottom(main_transform.origin, bottom_left, bottom_right);
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Figure out our near plane points
+
+	// 12. Create a near plane using -camera z and the eye further along in that axis.
+	Plane near;
+	Vector3 neg_z = -z;
+	if (neg_z.dot(p_transforms[1].origin) < neg_z.dot(p_transforms[0].origin)) {
+		near = Plane(p_transforms[0].origin, neg_z);
+	} else {
+		near = Plane(p_transforms[1].origin, neg_z);
+	}
+
+	// 13. Intersect near plane with bottm/left planes, to obtain min_vec then top/right to obtain max_vec
+	Vector3 min_vec;
+	ERR_FAIL_COND_MSG(
+			!near.intersect_3(bottom, planes[0][CameraMatrix::PLANE_LEFT], &min_vec), "Can't determine left camera near/left/bottom vector");
+	ERR_FAIL_COND_MSG(
+			!near.intersect_3(bottom, planes[1][CameraMatrix::PLANE_LEFT], &other), "Can't determine right camera near/left/bottom vector");
+	if (x.dot(other) < x.dot(min_vec)) {
+		min_vec = other;
+	}
+
+	Vector3 max_vec;
+	ERR_FAIL_COND_MSG(
+			!near.intersect_3(top, planes[0][CameraMatrix::PLANE_RIGHT], &max_vec), "Can't determine left camera near/right/top vector");
+	ERR_FAIL_COND_MSG(
+			!near.intersect_3(top, planes[1][CameraMatrix::PLANE_RIGHT], &other), "Can't determine right camera near/right/top vector");
+	if (x.dot(max_vec) < x.dot(other)) {
+		max_vec = other;
+	}
+
+	// 14. transform these points by the inverse camera to obtain local_min_vec and local_max_vec
+	Vector3 local_min_vec = main_transform_inv.xform(min_vec);
+	Vector3 local_max_vec = main_transform_inv.xform(max_vec);
+
+	// 15. get x and y from these to obtain left, top, right bottom for the frustum. Get the distance from near plane to camera origin to obtain near, and the distance from the far plane to the camer origin to obtain far.
+	float z_near = -near.distance_to(main_transform.origin);
+	float z_far = -far.distance_to(main_transform.origin);
+
+	// 16. Use this to build the combined camera matrix.
+	main_projection.set_frustum(local_min_vec.x, local_max_vec.x, local_min_vec.y, local_max_vec.y, z_near, z_far);
+
+	/////////////////////////////////////////////////////////////////////////////
+	// 3. now create new projection matrices that encompass repositioning
+	for (uint32_t v = 0; v < view_count; v++) {
+		Transform view_offset = p_transforms[v] * main_transform_inv;
+		view_projection[v] = p_projections[v] * CameraMatrix(view_offset);
+	}
+}

servers/rendering/renderer_scene_render.h
@@ -35,6 +35,8 @@
 #include "core/templates/paged_array.h"
 #include "servers/rendering/renderer_storage.h"
 
+#define RENDER_MAX_VIEWS 2
+
 class RendererSceneRender {
 public:
 	enum {
@@ -216,7 +218,22 @@ public:
 		uint32_t positional_light_count;
 	};
 
-	virtual void render_scene(RID p_render_buffers, const Transform &p_cam_transform, const CameraMatrix &p_cam_projection, bool p_cam_ortogonal, const PagedArray<GeometryInstance *> &p_instances, const PagedArray<RID> &p_lights, const PagedArray<RID> &p_reflection_probes, const PagedArray<RID> &p_gi_probes, const PagedArray<RID> &p_decals, const PagedArray<RID> &p_lightmaps, RID p_environment, RID p_camera_effects, RID p_shadow_atlas, RID p_occluder_debug_tex, RID p_reflection_atlas, RID p_reflection_probe, int p_reflection_probe_pass, float p_screen_lod_threshold, const RenderShadowData *p_render_shadows, int p_render_shadow_count, const RenderSDFGIData *p_render_sdfgi_regions, int p_render_sdfgi_region_count, const RenderSDFGIUpdateData *p_sdfgi_update_data = nullptr) = 0;
+	struct ViewData {
+		// flags
+		uint32_t view_count;
+		bool is_ortogonal;
+		bool vaspect;
+
+		// Main/center projection
+		Transform main_transform;
+		CameraMatrix main_projection;
+		CameraMatrix view_projection[RENDER_MAX_VIEWS];
+
+		void set_single_view(const Transform p_transform, const CameraMatrix p_projection, bool p_is_ortogonal, bool p_vaspect);
+		void set_stereo_view(uint32_t p_view_count, const Transform *p_transforms, const CameraMatrix *p_projections, bool p_is_ortogonal, bool p_vaspect);
+	};
+
+	virtual void render_scene(RID p_render_buffers, const ViewData *p_view_data, const PagedArray<GeometryInstance *> &p_instances, const PagedArray<RID> &p_lights, const PagedArray<RID> &p_reflection_probes, const PagedArray<RID> &p_gi_probes, const PagedArray<RID> &p_decals, const PagedArray<RID> &p_lightmaps, RID p_environment, RID p_camera_effects, RID p_shadow_atlas, RID p_occluder_debug_tex, RID p_reflection_atlas, RID p_reflection_probe, int p_reflection_probe_pass, float p_screen_lod_threshold, const RenderShadowData *p_render_shadows, int p_render_shadow_count, const RenderSDFGIData *p_render_sdfgi_regions, int p_render_sdfgi_region_count, const RenderSDFGIUpdateData *p_sdfgi_update_data = nullptr) = 0;
 
 	virtual void render_material(const Transform &p_cam_transform, const CameraMatrix &p_cam_projection, bool p_cam_ortogonal, const PagedArray<GeometryInstance *> &p_instances, RID p_framebuffer, const Rect2i &p_region) = 0;
 	virtual void render_particle_collider_heightfield(RID p_collider, const Transform &p_transform, const PagedArray<GeometryInstance *> &p_instances) = 0;
@@ -226,7 +243,7 @@ public:
 	virtual void set_debug_draw_mode(RS::ViewportDebugDraw p_debug_draw) = 0;
 
 	virtual RID render_buffers_create() = 0;
-	virtual void render_buffers_configure(RID p_render_buffers, RID p_render_target, int p_width, int p_height, RS::ViewportMSAA p_msaa, RS::ViewportScreenSpaceAA p_screen_space_aa, bool p_use_debanding) = 0;
+	virtual void render_buffers_configure(RID p_render_buffers, RID p_render_target, int p_width, int p_height, RS::ViewportMSAA p_msaa, RS::ViewportScreenSpaceAA p_screen_space_aa, bool p_use_debanding, uint32_t p_view_count) = 0;
 	virtual void gi_set_use_half_resolution(bool p_enable) = 0;
 
 	virtual void screen_space_roughness_limiter_set_active(bool p_enable, float p_amount, float p_limit) = 0;

cardboardcomputer/godot

servers/rendering/renderer_scene.h
@@ -189,7 +189,7 @@ public:
 
 	virtual RID render_buffers_create() = 0;
 
-	virtual void render_buffers_configure(RID p_render_buffers, RID p_render_target, int p_width, int p_height, RS::ViewportMSAA p_msaa, RS::ViewportScreenSpaceAA p_screen_space_aa, bool p_use_debanding) = 0;
+	virtual void render_buffers_configure(RID p_render_buffers, RID p_render_target, int p_width, int p_height, RS::ViewportMSAA p_msaa, RS::ViewportScreenSpaceAA p_screen_space_aa, bool p_use_debanding, uint32_t p_view_count) = 0;
 
 	virtual void gi_set_use_half_resolution(bool p_enable) = 0;
 
@@ -201,8 +201,7 @@ public:
 	virtual void sdfgi_set_debug_probe_select(const Vector3 &p_position, const Vector3 &p_dir) = 0;
 
 	virtual void render_empty_scene(RID p_render_buffers, RID p_scenario, RID p_shadow_atlas) = 0;
-	virtual void render_camera(RID p_render_buffers, RID p_camera, RID p_scenario, RID p_viewport, Size2 p_viewport_size, float p_lod_threshold, RID p_shadow_atlas) = 0;
-	virtual void render_camera(RID p_render_buffers, Ref<XRInterface> &p_interface, XRInterface::Eyes p_eye, RID p_camera, RID p_scenario, RID p_viewport, Size2 p_viewport_size, float p_lod_threshold, RID p_shadow_atlas) = 0;
+	virtual void render_camera(RID p_render_buffers, RID p_camera, RID p_scenario, RID p_viewport, Size2 p_viewport_size, float p_lod_threshold, RID p_shadow_atlas, Ref<XRInterface> &p_xr_interface) = 0;
 
 	virtual void update() = 0;
 	virtual void render_probes() = 0;

servers/rendering/renderer_scene_render.cpp
@@ -29,3 +29,153 @@
 /*************************************************************************/
 
 #include "renderer_scene_render.h"
+
+void RendererSceneRender::CameraData::set_camera(const Transform3D p_transform, const CameraMatrix p_projection, bool p_is_ortogonal, bool p_vaspect) {
+	view_count = 1;
+	is_ortogonal = p_is_ortogonal;
+	vaspect = p_vaspect;
+
+	main_transform = p_transform;
+	main_projection = p_projection;
+
+	view_offset[0] = Transform3D();
+	view_projection[0] = p_projection;
+}
+
+void RendererSceneRender::CameraData::set_multiview_camera(uint32_t p_view_count, const Transform3D *p_transforms, const CameraMatrix *p_projections, bool p_is_ortogonal, bool p_vaspect) {
+	ERR_FAIL_COND_MSG(p_view_count != 2, "Incorrect view count for stereoscopic view");
+
+	view_count = p_view_count;
+	is_ortogonal = p_is_ortogonal;
+	vaspect = p_vaspect;
+	Vector<Plane> planes[2];
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Figure out our center transform
+
+	// 1. obtain our planes
+	for (uint32_t v = 0; v < view_count; v++) {
+		planes[v] = p_projections[v].get_projection_planes(p_transforms[v]);
+	}
+
+	// 2. average and normalize plane normals to obtain z vector, cross them to obtain y vector, and from there the x vector for combined camera basis.
+	Vector3 n0 = planes[0][CameraMatrix::PLANE_LEFT].normal;
+	Vector3 n1 = planes[1][CameraMatrix::PLANE_RIGHT].normal;
+	Vector3 z = (n0 + n1).normalized();
+	Vector3 y = n0.cross(n1).normalized();
+	Vector3 x = y.cross(z).normalized();
+	y = z.cross(x).normalized();
+	main_transform.basis.set(x, y, z);
+
+	// 3. create a horizon plane with one of the eyes and the up vector as normal.
+	Plane horizon(p_transforms[0].origin, y);
+
+	// 4. Intersect horizon, left and right to obtain the combined camera origin.
+	ERR_FAIL_COND_MSG(
+			!horizon.intersect_3(planes[0][CameraMatrix::PLANE_LEFT], planes[1][CameraMatrix::PLANE_RIGHT], &main_transform.origin), "Can't determine camera origin");
+
+	// handy to have the inverse of the transform we just build
+	Transform3D main_transform_inv = main_transform.inverse();
+
+	// 5. figure out far plane, this could use some improvement, we may have our far plane too close like this, not sure if this matters
+	Vector3 far_center = (planes[0][CameraMatrix::PLANE_FAR].center() + planes[1][CameraMatrix::PLANE_FAR].center()) * 0.5;
+	Plane far(far_center, -z);
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Figure out our top/bottom planes
+
+	// 6. Intersect far and left planes with top planes from both eyes, save the point with highest y as top_left.
+	Vector3 top_left, other;
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[0][CameraMatrix::PLANE_LEFT], planes[0][CameraMatrix::PLANE_TOP], &top_left), "Can't determine left camera far/left/top vector");
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[1][CameraMatrix::PLANE_LEFT], planes[1][CameraMatrix::PLANE_TOP], &other), "Can't determine right camera far/left/top vector");
+	if (y.dot(top_left) < y.dot(other)) {
+		top_left = other;
+	}
+
+	// 7. Intersect far and left planes with bottom planes from both eyes, save the point with lowest y as bottom_left.
+	Vector3 bottom_left;
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[0][CameraMatrix::PLANE_LEFT], planes[0][CameraMatrix::PLANE_BOTTOM], &bottom_left), "Can't determine left camera far/left/bottom vector");
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[1][CameraMatrix::PLANE_LEFT], planes[1][CameraMatrix::PLANE_BOTTOM], &other), "Can't determine right camera far/left/bottom vector");
+	if (y.dot(other) < y.dot(bottom_left)) {
+		bottom_left = other;
+	}
+
+	// 8. Intersect far and right planes with top planes from both eyes, save the point with highest y as top_right.
+	Vector3 top_right;
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[0][CameraMatrix::PLANE_RIGHT], planes[0][CameraMatrix::PLANE_TOP], &top_right), "Can't determine left camera far/right/top vector");
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[1][CameraMatrix::PLANE_RIGHT], planes[1][CameraMatrix::PLANE_TOP], &other), "Can't determine right camera far/right/top vector");
+	if (y.dot(top_right) < y.dot(other)) {
+		top_right = other;
+	}
+
+	//  9. Intersect far and right planes with bottom planes from both eyes, save the point with lowest y as bottom_right.
+	Vector3 bottom_right;
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[0][CameraMatrix::PLANE_RIGHT], planes[0][CameraMatrix::PLANE_BOTTOM], &bottom_right), "Can't determine left camera far/right/bottom vector");
+	ERR_FAIL_COND_MSG(
+			!far.intersect_3(planes[1][CameraMatrix::PLANE_RIGHT], planes[1][CameraMatrix::PLANE_BOTTOM], &other), "Can't determine right camera far/right/bottom vector");
+	if (y.dot(other) < y.dot(bottom_right)) {
+		bottom_right = other;
+	}
+
+	// 10. Create top plane with these points: camera origin, top_left, top_right
+	Plane top(main_transform.origin, top_left, top_right);
+
+	// 11. Create bottom plane with these points: camera origin, bottom_left, bottom_right
+	Plane bottom(main_transform.origin, bottom_left, bottom_right);
+
+	/////////////////////////////////////////////////////////////////////////////
+	// Figure out our near plane points
+
+	// 12. Create a near plane using -camera z and the eye further along in that axis.
+	Plane near;
+	Vector3 neg_z = -z;
+	if (neg_z.dot(p_transforms[1].origin) < neg_z.dot(p_transforms[0].origin)) {
+		near = Plane(p_transforms[0].origin, neg_z);
+	} else {
+		near = Plane(p_transforms[1].origin, neg_z);
+	}
+
+	// 13. Intersect near plane with bottm/left planes, to obtain min_vec then top/right to obtain max_vec
+	Vector3 min_vec;
+	ERR_FAIL_COND_MSG(
+			!near.intersect_3(bottom, planes[0][CameraMatrix::PLANE_LEFT], &min_vec), "Can't determine left camera near/left/bottom vector");
+	ERR_FAIL_COND_MSG(
+			!near.intersect_3(bottom, planes[1][CameraMatrix::PLANE_LEFT], &other), "Can't determine right camera near/left/bottom vector");
+	if (x.dot(other) < x.dot(min_vec)) {
+		min_vec = other;
+	}
+
+	Vector3 max_vec;
+	ERR_FAIL_COND_MSG(
+			!near.intersect_3(top, planes[0][CameraMatrix::PLANE_RIGHT], &max_vec), "Can't determine left camera near/right/top vector");
+	ERR_FAIL_COND_MSG(
+			!near.intersect_3(top, planes[1][CameraMatrix::PLANE_RIGHT], &other), "Can't determine right camera near/right/top vector");
+	if (x.dot(max_vec) < x.dot(other)) {
+		max_vec = other;
+	}
+
+	// 14. transform these points by the inverse camera to obtain local_min_vec and local_max_vec
+	Vector3 local_min_vec = main_transform_inv.xform(min_vec);
+	Vector3 local_max_vec = main_transform_inv.xform(max_vec);
+
+	// 15. get x and y from these to obtain left, top, right bottom for the frustum. Get the distance from near plane to camera origin to obtain near, and the distance from the far plane to the camer origin to obtain far.
+	float z_near = -near.distance_to(main_transform.origin);
+	float z_far = -far.distance_to(main_transform.origin);
+
+	// 16. Use this to build the combined camera matrix.
+	main_projection.set_frustum(local_min_vec.x, local_max_vec.x, local_min_vec.y, local_max_vec.y, z_near, z_far);
+
+	/////////////////////////////////////////////////////////////////////////////
+	// 3. Copy our view data
+	for (uint32_t v = 0; v < view_count; v++) {
+		view_offset[v] = p_transforms[v] * main_transform_inv;
+		view_projection[v] = p_projections[v] * CameraMatrix(view_offset[v]);
+	}
+}

servers/rendering/renderer_scene_render.h
@@ -39,7 +39,8 @@ class RendererSceneRender {
 public:
 	enum {
 		MAX_DIRECTIONAL_LIGHTS = 8,
-		MAX_DIRECTIONAL_LIGHT_CASCADES = 4
+		MAX_DIRECTIONAL_LIGHT_CASCADES = 4,
+		MAX_RENDER_VIEWS = 2
 	};
 
 	struct GeometryInstance {
@@ -216,7 +217,24 @@ public:
 		uint32_t positional_light_count;
 	};
 
-	virtual void render_scene(RID p_render_buffers, const Transform3D &p_cam_transform, const CameraMatrix &p_cam_projection, bool p_cam_ortogonal, const PagedArray<GeometryInstance *> &p_instances, const PagedArray<RID> &p_lights, const PagedArray<RID> &p_reflection_probes, const PagedArray<RID> &p_voxel_gi_instances, const PagedArray<RID> &p_decals, const PagedArray<RID> &p_lightmaps, RID p_environment, RID p_camera_effects, RID p_shadow_atlas, RID p_occluder_debug_tex, RID p_reflection_atlas, RID p_reflection_probe, int p_reflection_probe_pass, float p_screen_lod_threshold, const RenderShadowData *p_render_shadows, int p_render_shadow_count, const RenderSDFGIData *p_render_sdfgi_regions, int p_render_sdfgi_region_count, const RenderSDFGIUpdateData *p_sdfgi_update_data = nullptr) = 0;
+	struct CameraData {
+		// flags
+		uint32_t view_count;
+		bool is_ortogonal;
+		bool vaspect;
+
+		// Main/center projection
+		Transform3D main_transform;
+		CameraMatrix main_projection;
+
+		Transform3D view_offset[RendererSceneRender::MAX_RENDER_VIEWS];
+		CameraMatrix view_projection[RendererSceneRender::MAX_RENDER_VIEWS];
+
+		void set_camera(const Transform3D p_transform, const CameraMatrix p_projection, bool p_is_ortogonal, bool p_vaspect);
+		void set_multiview_camera(uint32_t p_view_count, const Transform3D *p_transforms, const CameraMatrix *p_projections, bool p_is_ortogonal, bool p_vaspect);
+	};
+
+	virtual void render_scene(RID p_render_buffers, const CameraData *p_camera_data, const PagedArray<GeometryInstance *> &p_instances, const PagedArray<RID> &p_lights, const PagedArray<RID> &p_reflection_probes, const PagedArray<RID> &p_voxel_gi_instances, const PagedArray<RID> &p_decals, const PagedArray<RID> &p_lightmaps, RID p_environment, RID p_camera_effects, RID p_shadow_atlas, RID p_occluder_debug_tex, RID p_reflection_atlas, RID p_reflection_probe, int p_reflection_probe_pass, float p_screen_lod_threshold, const RenderShadowData *p_render_shadows, int p_render_shadow_count, const RenderSDFGIData *p_render_sdfgi_regions, int p_render_sdfgi_region_count, const RenderSDFGIUpdateData *p_sdfgi_update_data = nullptr) = 0;
 
 	virtual void render_material(const Transform3D &p_cam_transform, const CameraMatrix &p_cam_projection, bool p_cam_ortogonal, const PagedArray<GeometryInstance *> &p_instances, RID p_framebuffer, const Rect2i &p_region) = 0;
 	virtual void render_particle_collider_heightfield(RID p_collider, const Transform3D &p_transform, const PagedArray<GeometryInstance *> &p_instances) = 0;
@@ -226,7 +244,7 @@ public:
 	virtual void set_debug_draw_mode(RS::ViewportDebugDraw p_debug_draw) = 0;
 
 	virtual RID render_buffers_create() = 0;
-	virtual void render_buffers_configure(RID p_render_buffers, RID p_render_target, int p_width, int p_height, RS::ViewportMSAA p_msaa, RS::ViewportScreenSpaceAA p_screen_space_aa, bool p_use_debanding) = 0;
+	virtual void render_buffers_configure(RID p_render_buffers, RID p_render_target, int p_width, int p_height, RS::ViewportMSAA p_msaa, RS::ViewportScreenSpaceAA p_screen_space_aa, bool p_use_debanding, uint32_t p_view_count) = 0;
 	virtual void gi_set_use_half_resolution(bool p_enable) = 0;
 
 	virtual void screen_space_roughness_limiter_set_active(bool p_enable, float p_amount, float p_limit) = 0;
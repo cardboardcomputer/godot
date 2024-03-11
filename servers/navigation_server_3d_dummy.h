43c8c720152253986e413b6020fcf219175bceaa
tamas <tamas@xndu.net>
2024-03-11 17:27:06 -0500

    Added NavigationServer3D.map_get_closest_point_info()
    Often both the position and normal for a navigation point is needed at
    the same time. This function returns all information available at the
    at once so multiple queries against the map regions/polygons don't
    need to be made, i.e consecutive calls to `map_get_closest_point()`
    and then `map_get_closest_point_normal()`, which would otherwise be
    necessary in GDScript.

--- servers/navigation_server_3d_dummy.h
+++ servers/navigation_server_3d_dummy.h
@@ -57,6 +57,7 @@ public:
 	Vector3 map_get_closest_point_to_segment(RID p_map, const Vector3 &p_from, const Vector3 &p_to, const bool p_use_collision) const override { return Vector3(); }
 	Vector3 map_get_closest_point(RID p_map, const Vector3 &p_point) const override { return Vector3(); }
 	Vector3 map_get_closest_point_normal(RID p_map, const Vector3 &p_point) const override { return Vector3(); }
+	Dictionary map_get_closest_point_info(RID p_map, const Vector3 &p_point) const override { return Dictionary(); }
 	RID map_get_closest_point_owner(RID p_map, const Vector3 &p_point) const override { return RID(); }
 	TypedArray<RID> map_get_links(RID p_map) const override { return TypedArray<RID>(); }
 	TypedArray<RID> map_get_regions(RID p_map) const override { return TypedArray<RID>(); }
